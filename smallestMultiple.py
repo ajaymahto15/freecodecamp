665a7277aac7860fed746be210921d37b536276e
Ajay Kumar Mahto <165130037+ajaymahto15@users.noreply.github.com>
2024-11-23 07:13:35 +0000

    Problem 5: Smallest multiple

---
+++ smallestMultiple.py
@@ -0,0 +1,18 @@
+# Problem 5: Smallest multiple
+# 
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# 
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to n?
+# Tests
+# Waiting: 1. smallestMult(5) should return a number.
+# Waiting: 2. smallestMult(5) should return 60.
+# Waiting: 3. smallestMult(7) should return 420.
+# Waiting: 4. smallestMult(10) should return 2520.
+# Waiting: 5. smallestMult(13) should return 360360.
+# Waiting: 6. smallestMult(20) should return 232792560.
+
+def main():
+    
+
+if __name__ == '__main__':
+    main()
